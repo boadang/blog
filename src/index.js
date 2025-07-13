c894760e454b22d3f70487b265fa7bc355e1970a
baodangdz1@gmail.com <baodangdz1@gmail.com>
2025-07-13 22:48:43 +0700

    add ignore git

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const exhbs = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+//HTTP logger
+app.use(morgan('combined'));
+
+//Template engine setup
+app.engine('handlebars', exhbs.engine());
+
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.listen (port, () => console.log(`Server is running on http://localhost:${port}`));
