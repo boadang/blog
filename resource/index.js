6958276873d42b846f052e89558fb754b78fae5e
Bao Dang <baodangdz1@gmail.com>
2025-07-15 10:50:39 +0700

    remove public/css folder

--- resource/index.js
+++ resource/index.js
@@ -6,17 +6,25 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 //HTTP logger
 app.use(morgan('combined'));
 
 //Template engine setup
-app.engine('handlebars', exhbs.engine());
+app.engine('hbs', exhbs.engine({
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'src/views/layouts'),
+    defaultLayout: 'main',
+}));
 
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'src/views'));
 
 app.get('/', (req, res) => {
     res.render('home');
 })
-
+app.get('/news', (req, res) => {
+    res.render('news');
+});
 app.listen (port, () => console.log(`Server is running on http://localhost:${port}`));
