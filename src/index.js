4b6b0103365cbad1d878db0047388e832a7eaab9
baodangdz1@gmail.com <baodangdz1@gmail.com>
2025-07-29 08:23:40 +0700

    old version on PC

--- src/index.js
+++ src/index.js
@@ -9,14 +9,20 @@ const port = 3000;
 //HTTP logger
 app.use(morgan('combined'));
 
-//Template engine setup
-app.engine('handlebars', exhbs.engine());
+// Template engine setup
+app.engine('hbs', exhbs.engine({
+    extname: '.hbs',
+}));
 
-app.set('view engine', 'handlebars');
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
     res.render('home');
-})
+});
 
-app.listen (port, () => console.log(`Server is running on http://localhost:${port}`));
+app.get('/news', (req, res) => {
+    res.render('news');
+});
+
+app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
